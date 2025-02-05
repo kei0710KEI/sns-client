b42e81a231d0ff71ac3562eed004cfee8bd65f58
kei0710KEI <keisuke0710.taka@gmail.com>
2025-02-05 15:02:26 +0900

    baseUrl

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
     // baseURL: "http://localhost:5000/api",
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
     headers: {
         "Connect-Type": "application/json",
     },
